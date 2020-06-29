3fc523259b4a817919e0ea51662bbf23ce8b1235
Shreyas Kousik <skousik@umich.edu>
2020-06-29 14:58:01 -0400

    Update check_if_plot_is_available.m

--- src/utility/plotting/check_if_plot_is_available.m
+++ src/utility/plotting/check_if_plot_is_available.m
@@ -7,19 +7,51 @@ function out = check_if_plot_is_available(simulator_object,fieldname)
     % figure root. If both the figure and the object's plot data are
     % available, return true.
     %
+    % If simulator_object.plot_data.(fieldname) contains a cell array of
+    % n plotted object handles, this function checks each object in the
+    % cell array.
+    %
     % Author: Shreyas Kousik
     % Created: shrug
     % Updated: 19 Mar 2020
     
+    % if no fieldname is passed in, check the first fieldname in the
+    % object's plot_data property
     if nargin < 2
         F = fieldnames(simulator_object.plot_data) ;
         fieldname = F{1} ;
     end
+    
     try
+        % try to get the data in the given field (if the field doesn't
+        % exist, this will throw an error)
         h = simulator_object.plot_data.(fieldname) ;
+        
+        % get the current figure (if there is no figure up, then this will
+        % be an empty GraphicsPlaceholder array)
         fh = get(groot,'CurrentFigure') ;
-        out = ~(isempty(h) || ~all(isvalid(h)) || isempty(fh)) ;
+        
+        % check if plot is available:
+        if iscell(h)
+            % if h is a cell array of plotted objects, check each one
+            out = false(1,length(h)) ;
+            
+            if ~isempty(fh) % if the figure is up...
+                for idx = 1:length(h)
+                    % iterate through the plotted objects in h, and check
+                    % that each one is nonempty and a valid plot object
+                    out(idx) = ~(isempty(h{idx}) || ~all(isvalid(h{idx}))) ;
+                end
+            end
+        else
+            % otherwise, just check if h is nonempty and valid (meaning
+            % that the object has been plotted correctly) and if the figure
+            % is up (meaning fh is nonempty)
+            out = ~(isempty(h) || ~all(isvalid(h)) || isempty(fh)) ;
+        end
     catch
+        % the only way anything above should throw an error is if h is an
+        % invalid fieldname, hence the following warning:
         warning([fieldname,' may not be not a valid plot_data field of the ',...
             simulator_object.name, ' object'])
         out = false ;
