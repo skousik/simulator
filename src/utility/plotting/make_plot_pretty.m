12e8f2b44ce1aa4b802ae884bdf2f31a7bb123f6
Shreyas Kousik <shreyas.kousik@gmail.com>
2021-05-14 15:31:45 -0700

    Create make_plot_pretty.m

---
+++ src/utility/plotting/make_plot_pretty.m
@@ -0,0 +1,10 @@
+function make_plot_pretty()
+% make_plot_pretty()
+%
+% This sets things the way Shreyas likes 'em.
+%
+% Authors: Shreyas Kousik
+% Created: 6 May 2021
+    set_plot_linewidths(1.5)
+    set_plot_fontsize(15)
+end
